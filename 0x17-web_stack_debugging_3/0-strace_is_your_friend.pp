5057a59326c0d4f2a454e3811dacc368f2769366
Allan <allancheruiyot003@gmail.com>
2024-05-08 16:25:03 +0000

    ALX SE

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,44 +1,6 @@
-class strace_apache {
-  package { 'strace':
-    ensure => installed,
-  }
+# Fixes an error 500 on the apache server
 
-  package { 'apache2':
-    ensure => installed,
-  }
-
-  service { 'apache2':
-    ensure => running,
-    enable => true,
-  }
-
-  file { '/etc/apache2/apache2.conf':
-    owner => 'root',
-    group => 'root',
-    mode => '644',
-    notify => Service['apache2'],
-  }
-
-  file { '/etc/apache2/mods-enabled/rewrite.load':
-    owner => 'root',
-    group => 'root',
-    mode => '644',
-    notify => Service['apache2'],
-  }
-
-  exec { 'strace_apache':
-    command => '/usr/bin/strace -p $(pgrep -f apache) -o /tmp/strace.log',
-    user   => 'root',
-    require=> Package['strace'],
-  }
-
-  file { '/etc/apache2/sites-available/default':
-    owner   => 'root',
-    group   => 'root',
-    mode   => '644',
-    content =>
-      "ServerName localhost\n" +
-      "ProxyPass / http://127.0.0.1:8080/\n" +
-      "ProxyPassReverse / http://127.0.0.1:8080/\n",
-  }
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
